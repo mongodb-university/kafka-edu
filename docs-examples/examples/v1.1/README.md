c00bfbb7345a4704bc808296c91380769384e5a2
Aleksander Binion <alek.binion@mongodb.com>
2021-10-26 21:51:09 -0400

    cc - feedback

--- docs-examples/examples/v1.1/README.md
+++ docs-examples/examples/v1.1/README.md
@@ -1,6 +1,5 @@
 # Unsupported Connector Version
 
 The [MongoDB Kafka Connector Documentation](https://docs.mongodb.com/kafka-connector/current/)
-quick start and tutorials do not support v1.1 of the connector. The quick start and tutorials depend on the
-`change.data.capture.handler` feature of the connector. The connector supports
-the `change.data.capture.handler` feature in v1.4 and later.
+quick start and tutorials do not support v1.1 of the connector. The quick start and tutorials depend on the Change Data Capture Handler
+feature of the connector which is available in the connector v1.4 and later.
--- docs-examples/scripts/build.py
+++ docs-examples/scripts/build.py
@@ -5,13 +5,13 @@ import fnmatch
 import shutil
 
 REPLACE_CONSTANT="MONGODB_KAFKA_CONNECTOR_VERSION"
-SRC_DIR = "../source"
-BUILD_DIR = "../examples"
-UNSUPPORTED_DIR = "../unsupported"
-ABS_SRC = os.path.abspath(SRC_DIR)
-ABS_UNSUPPORTED = os.path.abspath(UNSUPPORTED_DIR)
-UNSUPPORTED_REPLACE_REGEX = "README.md"
-SOURCE_REPLACE_REGEX = "[Dd]ocker*"
+RELATIVE_SRC_DIR = "../source"
+RELATIVE_BUILD_DIR = "../examples"
+RELATIVE_UNSUPPORTED_DIR = "../unsupported"
+SRC_DIR = os.path.abspath(RELATIVE_SRC_DIR)
+UNSUPPORTED_DIR = os.path.abspath(RELATIVE_UNSUPPORTED_DIR)
+README_UNIX_PATTERN = "README.md"
+DOCKER_UNIX_PATTERN = "[Dd]ocker*"
 
 IS_SUPPORTED = "is_supported"
 CONFLUENT_VERSION = "confluent_hub_version"
@@ -23,41 +23,41 @@ REPLACE_MAP = {
     "v1.6": {IS_SUPPORTED:True, CONFLUENT_VERSION:"1.6.1"},
     "v1.5": {IS_SUPPORTED:True, CONFLUENT_VERSION:"1.5.1"},
     "v1.4": {IS_SUPPORTED:True, CONFLUENT_VERSION:"1.4.0"},
-    "v1.3": {IS_SUPPORTED:False, CONFLUENT_VERSION:None},
-    "v1.2": {IS_SUPPORTED:False, CONFLUENT_VERSION:None},
-    "v1.1": {IS_SUPPORTED:False, CONFLUENT_VERSION:None},
+    "v1.3": {IS_SUPPORTED:False, CONFLUENT_VERSION:"1.3.0"},
+    "v1.2": {IS_SUPPORTED:False, CONFLUENT_VERSION:"1.2.0"},
+    "v1.1": {IS_SUPPORTED:False, CONFLUENT_VERSION:"1.1.0"},
     "v1.0": {IS_SUPPORTED:False, CONFLUENT_VERSION:None}
 }
 
 def recursively_find_replace(directory, find, replace, file_pattern):
-    '''Recursively find and replace a string in a directory.'''
+    '''Recursively searches directories for all occurrences of a string and replaces them.'''
     for path, _, files in os.walk(os.path.abspath(directory)):
         for filename in fnmatch.filter(files, file_pattern):
             filepath = os.path.join(path, filename)
-            with open(filepath, "r", encoding="UTF-8") as f_1:
-                file_contents = f_1.read()
+            with open(filepath, "r", encoding="UTF-8") as input_file:
+                file_contents = input_file.read()
             file_contents = file_contents.replace(find, replace)
-            with open(filepath, "w", encoding="UTF-8") as f_2:
-                f_2.write(file_contents)
+            with open(filepath, "w", encoding="UTF-8") as output_file:
+                output_file.write(file_contents)
 
 def copy_replace_pipeline_source(dst, replace):
     '''Copy pipeline source and replace a string constant.'''
-    shutil.copytree(ABS_SRC, dst)
-    recursively_find_replace(dst, REPLACE_CONSTANT, replace, SOURCE_REPLACE_REGEX)
+    shutil.copytree(SRC_DIR, dst)
+    recursively_find_replace(dst, REPLACE_CONSTANT, replace, DOCKER_UNIX_PATTERN)
 
 def copy_replace_unsupported_dir(dst, replace):
     '''Copy unsupported template and replace a string constant.'''
-    shutil.copytree(ABS_UNSUPPORTED, dst)
-    recursively_find_replace(dst, REPLACE_CONSTANT, replace, UNSUPPORTED_REPLACE_REGEX)
+    shutil.copytree(UNSUPPORTED_DIR, dst)
+    recursively_find_replace(dst, REPLACE_CONSTANT, replace, README_UNIX_PATTERN)
 
 def generate_tutorials():
-    '''Generate docs examples and READMEs for unsupported versions of the connector.'''
+    '''Generate examples for supported and unsupported versions of the connector.'''
     try:
-        shutil.rmtree(os.path.abspath(BUILD_DIR))
+        shutil.rmtree(os.path.abspath(RELATIVE_BUILD_DIR))
     except FileNotFoundError:
         pass
     for connector_version, support_dict in REPLACE_MAP.items():
-        abs_dst = os.path.abspath(os.path.join(BUILD_DIR, connector_version))
+        abs_dst = os.path.abspath(os.path.join(RELATIVE_BUILD_DIR, connector_version))
         if support_dict[IS_SUPPORTED]:
             copy_replace_pipeline_source(abs_dst, support_dict[CONFLUENT_VERSION])
         else:
