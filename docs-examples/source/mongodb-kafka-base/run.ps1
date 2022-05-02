89b4171d81d5abfb118d5689db95b208002e6331
Rob Walters <robert.walters@mongodb.com>
2022-05-02 14:49:38 -0400

    minor fixes for win powershell (#19)
    * minor fixes for win powershell
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Cho <chris.cho@10gen.com>
    
    Co-authored-by: Chris Cho <chris.cho@10gen.com>

--- docs-examples/source/mongodb-kafka-base/run.ps1
+++ docs-examples/source/mongodb-kafka-base/run.ps1
@@ -46,21 +46,20 @@ docker-compose up -d --build
 
 Write-Host  "Configuring the MongoDB ReplicaSet."
 
-docker-compose exec mongo1 /usr/bin/mongo --eval "if (rs.status()['ok'] == 0) { rsconf = { _id : 'rs0', members: [ { _id : 0, host : 'mongo1:27017', priority: 1.0 }, { _id : 1, host : 'mongo2:27017', priority: 0.5 }, { _id : 2, host : 'mongo3:27017', priority: 0.5 } ] }; rs.initiate(rsconf); } rs.conf();"
-
+docker-compose exec mongo1 /usr/bin/mongo --eval "rsconf = { _id : 'rs0', members: [ { _id : 0, host : 'mongo1:27017', priority: 1.0 }]}; rs.initiate(rsconf);"
 Write-Host  "
 
 ==============================================================================================================
 
 The following services are running:
 
-MongoDB 3-node cluster available on port 27017, 27018 27019
+MongoDB 3-node cluster available on port 27017
 Kafka Broker on 9092
 Kafka Zookeeper on 2181
 Kafka Connect on 8083
 
 Status of kafka connectors:
-sh status.h
+powershell.exe .\status.h
 
 To stop these serivces:
 docker-compose-down
@@ -69,12 +68,8 @@ To stop and remove the MongoDB database volumes:
 docker-compose-down -v
 
 An image has been created that includes all the client tools needed for the tutorials such as MongoShell, KafkaCat, etc:
-docker run --rm --name shell1 --network kafka-edu_localnet -it tutorialshell:0.1 bash
+docker run --rm --name shell1 --network mongodb-kafka-base_localnet -it robwma/mongokafkatutorial:latest bash
 
 
 ==============================================================================================================
 "
-
-# docker-compose down  -v
-
-# note: we use a -v to remove the volumes, else you'll end up with old data
